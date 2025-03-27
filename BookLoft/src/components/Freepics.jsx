44fd4bc1392333dfc8fc373ed79a1a5f603dfffd
Gaurav_Jha <jhagaurav82003@gmail.com>
2025-03-27 21:23:03 +0530

    recent commited

--- BookLoft/src/components/Freepics.jsx
+++ BookLoft/src/components/Freepics.jsx
@@ -1,48 +1,48 @@
-import {React,useState,useEffect} from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import Card from './Card';
-import axios from 'axios';
+import { React, useState, useEffect } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import Card from "./Card";
+import axios from "axios";
 
 const Freepics = () => {
-
   const [book, setBook] = useState([]);
-  
+
   useEffect(() => {
-      const getBook = async () => {
-          try {
-            const res = await axios.get("http://localhost:3000/book");
-            const filteredData = res.data.filter((item) => item.category === "free");
-            
-              setBook(filteredData);
-          } catch (err) {
-              console.log(err);
-          }
-      };
-      getBook();
-     
-  },[])
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/book");
+        const filteredData = res.data.filter(
+          (item) => item.category === "Free"
+        );
+
+        setBook(filteredData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getBook();
+  }, []);
 
   const mappedData = book.map((item) => (
-    <div className="px-4"> 
+    <div className="px-4">
       <Card item={item} key={item.id} />
     </div>
   ));
 
   const responsive = {
-    0: { items: 1 },  
-    500: { items: 2 }, 
-    1024: { items: 3 }, 
+    0: { items: 1 },
+    500: { items: 2 },
+    1024: { items: 3 },
   };
 
   return (
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
       <div>
         <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
-        <p >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
-          corporis nulla non suscipit, iure neque earum?
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          veritatis alias pariatur ad dolor repudiandae eligendi corporis nulla
+          non suscipit, iure neque earum?
         </p>
       </div>
 
@@ -52,11 +52,11 @@ const Freepics = () => {
           items={mappedData}
           responsive={responsive}
           controlsStrategy="alternate"
-          infinite={false} 
+          infinite={false}
           disableDotsControls={true}
           disableButtonsControls={false}
-          stagePadding={{ left: 20, right: 20 }} 
-          animationDuration={500} 
+          stagePadding={{ left: 20, right: 20 }}
+          animationDuration={500}
         />
       </div>
     </div>
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,11 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
 
-// const { getBook } = require('../controllers/bookController');
-
-// router.get('/', getBook);
-
-// export default router;
 const express = require('express');
 const router = express.Router();
 
